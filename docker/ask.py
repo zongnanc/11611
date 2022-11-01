bf1cd949fbe17b4aa62944f7bd48e5d9bf2536c8
lawrence.li <tiantonl@andrew.cmu.edu>
2022-10-31 23:41:33 -0400

    added docker container

--- docker/ask.py
+++ docker/ask.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 
 import sys
 
@@ -19,8 +21,6 @@ def get_question(context, max_length=64):
 
 def generation(context, n_questions=1):
     question_count = 0
-    output = open('output.txt', 'w')
-    output.close()
     text_file = open(context, 'r')
     context_str = text_file.read().replace('\n', '')
     sentences = list(map(str.strip, context_str.split(".")))
@@ -30,13 +30,13 @@ def generation(context, n_questions=1):
         if question_count >= len(sentences):
             break
         question = get_question(sentences[question_count])
-        output = open('output.txt', 'a')
-        output.writelines(question + "\n")
-        output.close()
+        question_num = question_count + 1
+        sys.stdout.write("Q" + str(question_num)+ " " + question + "\n")
         question_count += 1
 
 
 if __name__ == '__main__':
+
     context = sys.argv[1]
     n_questions = int(sys.argv[2])
     generation(context, n_questions)
